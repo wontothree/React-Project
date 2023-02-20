58be88ce1b0123cba6489f5369f364032295daef
Liam Kevin <prepared7913@gmail.com>
2023-02-21 01:15:12 +0900

    fix: validation details

--- web/src/pages/Signup.jsx
+++ web/src/pages/Signup.jsx
@@ -21,14 +21,13 @@ const Signup = () => {
         }));
     };
 
-
     const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     const isRegexEmail = emailRegex.test(userSignupData.userEmail) || userSignupData.userEmail.length === 0;
     const isRegexEmailMessage = isRegexEmail ? '' : "올바른 이메일 형식이 아닙니다.";
 
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    const isRegexPassword = passwordRegex.test(userSignupData.userPassword) || userSignupData.userPassword.length === 0;
-    const isRegexPasswordMessage = isRegexPassword ? '' : "위험 (영문자, 숫자, 특수문자 조합/ 8~20자)";
+    const isRegexPassword = passwordRegex.test(userSignupData.userPassword);
+    const isRegexPasswordMessage = isRegexPassword ? '안전' : "사용불가 (영문자, 숫자, 특수문자 조합/ 8~20자)";
 
     const isRegexPasswordConfirm = userSignupData.userPassword === userSignupData.userPasswordConfirm || userSignupData.userPasswordConfirm.length === 0;
     const isRegexPasswordConfirmMessage = isRegexPasswordConfirm ? '' : "비밀번호가 일치하지 않습니다."
@@ -49,7 +48,8 @@ const Signup = () => {
             <h1 style={{size:20}}>회원가입</h1>
             <form>
                 <Formbox htmlFor="userEmail" name="이메일*" type="email" id="userEmail" title="이메일" onChange={onChangeInput} maxLength='30' message={isRegexEmailMessage}/>
-                <Formbox htmlFor="userPassword" name="비밀번호*" type="password" id="userPassword" title="비밀번호(숫자, 영문자, 특수문자 조합/ 8~20자)" onChange={onChangeInput} maxLength='20' className={`message ${isRegexPassword ? 'success' : 'error'}`} message={isRegexPasswordMessage}/>
+                <Formbox htmlFor="userPassword" name="비밀번호*" type="password" id="userPassword" title="비밀번호(숫자, 영문자, 특수문자 조합/ 8~20자)" onChange={onChangeInput} maxLength='20'/>
+                {userSignupData.userPassword.length > 0 &&<div className={`message ${isRegexPassword ? 'success' : 'error'}`}>{isRegexPasswordMessage}</div>}
                 <Formbox htmlFor="userPasswordConfirm" name="비밀번호 확인*" type="password" id="userPasswordConfirm" title="비밀번호 확인" onChange={onChangeInput} maxLength='20' message={isRegexPasswordConfirmMessage}/>
                 <Formbox htmlFor="userName" name="이름*" type="text" id="userName" title="이름" onChange={onChangeInput} maxLength="10" message={isRegexNameMessage}/>
                 <Formbox htmlFor="userAge" name="생년월일" type="number" id="userAge" title="생년월일(6자)" onChange={onChangeInput}/>
