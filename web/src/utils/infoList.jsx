70dabb09389a0fd19d5cd4bb4d86439866879290
bada308 <gangbada890@gmail.com>
2023-02-23 19:15:36 +0900

    [feat] utils 폴더 추가, 정보 리스트 생성

---
+++ web/src/utils/infoList.jsx
@@ -0,0 +1,27 @@
+export const jobGroubList = [
+  "프론트엔드 개발자",
+  "백엔드 개발자",
+  "풀스택 개발자",
+  "데이터베이스 개발자",
+  "머신러닝 엔지니어",
+  "모바일 앱 개발자",
+  "QA 엔지니어",
+  "시스템 엔지니어",
+  "보안 엔지니어",
+  "소프트웨어 아키텍트",
+];
+
+export const teckStackList = [
+  "HTML/CSS",
+  "JavaScript",
+  "TypeScript",
+  "Java",
+  "Python",
+  "PHP",
+  "Ruby",
+  "Go",
+  "Rust",
+  "C",
+  "C++",
+  "C#",
+];
