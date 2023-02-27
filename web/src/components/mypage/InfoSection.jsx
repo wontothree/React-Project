e70ba5d0203c7b43ebe861e14061585aa8cadb8d
bada308 <gangbada890@gmail.com>
2023-02-27 19:26:49 +0900

    [feat]context 이용해서 데이터 전달, InfoSection 컴포넌트 수정

--- web/src/components/mypage/InfoSection.jsx
+++ web/src/components/mypage/InfoSection.jsx
@@ -1,17 +1,32 @@
 import EditButton from "./EditButton";
+import TextBox from "./TextBox";
+import React, { useContext } from "react";
+import { dataStateContext } from "../../App.jsx";
 
-const InfoSection = ({ name, className, buttonType, dataType }) => {
-  const dummyData = {
-    userNickname: ["닉네임"],
-    userCollege: ["OO대학교/oo캠퍼스/컴퓨터공학과/학사"],
-    userSkill: ["JavaScript", "React"],
-    userJob: [],
-    userExperience: [],
-    userCareer: [],
-    userIntroduction: [
-      "반갑습니다. OO백화점 영업관리부분에 지원한 OOO이라고 합니다./n저는 택배회사에서 6개월간 인턴하면서 ‘데이터의 중요성’을 깨달았습니다./nCS업무를 하다보니 OO지역에서 유난히 클레임이 많이 나온다는 것을 느꼈습니다. 그래서 최근 1년간 클레임 데이터를 추출하여 지역별로 정리해서 건수를 확인했습니다. 확인해보니 OO지역이  유난히 많았고, 해당 지점과 연락을 해보니 기사님이 건강상의 이유로 근태가 좋지않아 업무가 점점 쌓이고, 실수가 많아졌다는 것입니다.",
-    ],
-    userBlog: ["https://github.com/abc", "https://velog.io/abc"],
+const InfoSection = ({ name, className, buttonType, dataType, tagType }) => {
+  const dummyData = useContext(dataStateContext);
+
+  const tag = () => {
+    switch (tagType) {
+      case "span":
+        return dummyData[dataType].map((it) => {
+          return <span>{it}</span>;
+        });
+      case "a":
+        return dummyData[dataType].map((it) => {
+          return (
+            <a href={it} target="_blank" rel="noopener noreferrer">
+              {it}
+            </a>
+          );
+        });
+      case "TextBox":
+        return dummyData[dataType].map((it) => {
+          return <TextBox name={it} />;
+        });
+      default:
+        return;
+    }
   };
 
   return (
@@ -21,15 +36,7 @@ const InfoSection = ({ name, className, buttonType, dataType }) => {
           <span>{name}</span>
           <EditButton type={buttonType} />
         </header>
-        <section>
-          {dummyData[dataType].map((it) => {
-            return (
-              <a href={it} target="_blank" rel="noopener noreferrer">
-                {it}
-              </a>
-            );
-          })}
-        </section>
+        <section>{tag()}</section>
       </div>
     </>
   );
--- web/src/pages/MyPage.jsx
+++ web/src/pages/MyPage.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-import EditButton from "../components/Mypage/EditButton";
-import InfoSection from "../components/Mypage/InfoSection";
-import TextBox from "../components/Mypage/TextBox";
+import React, { createContext, useMemo, useContext } from "react";
+import InfoSection from "../components/mypage/InfoSection";
 import { teckStackList } from "../utils/infoList";
 
 const MyPage = () => {
@@ -13,6 +11,7 @@ const MyPage = () => {
           name={"프로필"}
           buttonType={"edit"}
           dataType={"userNickname"}
+          tagType={"span"}
         />
 
         <InfoSection
@@ -20,6 +19,7 @@ const MyPage = () => {
           name={"기술 블로그 / 깃허브"}
           buttonType={"append"}
           dataType={"userBlog"}
+          tagType={"a"}
         />
 
         <InfoSection
@@ -27,6 +27,7 @@ const MyPage = () => {
           name={"기술 스텍"}
           buttonType={"append"}
           dataType={"userSkill"}
+          tagType={"TextBox"}
         />
 
         <InfoSection
@@ -34,6 +35,7 @@ const MyPage = () => {
           name={"희망 직무"}
           buttonType={"append"}
           dataType={"userJob"}
+          tagType={"TextBox"}
         />
       </div>
 
@@ -43,6 +45,7 @@ const MyPage = () => {
           name={"자기소개"}
           buttonType={"edit"}
           dataType={"userIntroduction"}
+          tagType={"span"}
         />
 
         <InfoSection
@@ -50,6 +53,7 @@ const MyPage = () => {
           name={"학력"}
           buttonType={"append"}
           dataType={"userCollege"}
+          tagType={"span"}
         />
 
         <InfoSection
@@ -57,6 +61,7 @@ const MyPage = () => {
           name={"수상 경력/자격증"}
           buttonType={"append"}
           dataType={"userCareer"}
+          tagType={"span"}
         />
 
         <InfoSection
@@ -64,6 +69,7 @@ const MyPage = () => {
           name={"프로젝트 경험 / 실무 경험"}
           buttonType={"append"}
           dataType={"userExperience"}
+          tagType={"span"}
         />
       </div>
     </div>
