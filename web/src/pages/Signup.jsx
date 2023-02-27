6a4f9e3c603c98dab239e4a764ef628160c59e12
hinomaker <chh5946@gmail.com>
2023-02-27 20:12:51 +0900

    google api login, context data, google user picture

--- web/src/pages/Signup.jsx
+++ web/src/pages/Signup.jsx
@@ -1,87 +1,195 @@
-import React, { useState, useEffect } from 'react'
+import { UserContext } from "../App";
+import React, { useState, useEffect, useContext } from "react";
 
-import Button from '../components/Button';
-import Formbox from '../components/Formbox';
-import '../components/signup.css'
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Formbox from "../components/Formbox";
+import "../components/signup.css";
+
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 const Signup = () => {
-    const [userSignupData, setUserSignupData] = useState({
-        userEmail: '',
-        userPassword: '',
-        userPasswordConfirm: '',
-        userName: '',
-        userBirthdateYear: '',
-        userBirthdateMonth: '',
-        userBirthdateDay: '',
-        userSex: '',
-        userPhone: '',
-    });
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-    const onChangeInput = (e) => {
-        const { id, value } = e.target;
-        setUserSignupData((prev) => ({
-            ...prev,
-            [id]: value,
-        }));
-    };
+  const responseGoogle = async response => {
+    var decodedHeader = jwt_decode(response.credential);
+    console.log(decodedHeader);
+    const { email, name, picture } = decodedHeader;
 
-    useEffect(() => {
-        console.log(userSignupData)
-    })
+    setUser(prev => ({
+      ...prev,
+      userEmail: email,
+      userPassword: "googleLogin",
+      userPasswordConfirm: "googleLogin",
+      userName: name,
+      userPicture: picture,
+    }));
 
-    const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-    const isRegexEmail = emailRegex.test(userSignupData.userEmail) || userSignupData.userEmail.length === 0;
-    const isRegexEmailMessage = isRegexEmail ? '' : "올바른 이메일 형식이 아닙니다.";
+    navigate("/signupsecond");
+  };
 
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    const isRegexPassword = passwordRegex.test(userSignupData.userPassword);
-    const isRegexPasswordMessage = isRegexPassword ? '안전' : "사용불가 (영문자, 숫자, 특수문자 조합/ 8~20자)";
+  const onChangeInput = e => {
+    const { id, value } = e.target;
+    setUser(prev => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
 
-    const isRegexPasswordConfirm = userSignupData.userPassword === userSignupData.userPasswordConfirm || userSignupData.userPasswordConfirm.length === 0;
-    const isRegexPasswordConfirmMessage = isRegexPasswordConfirm ? '' : "비밀번호가 일치하지 않습니다."
+  //   useEffect(() => {
+  //     console.log(user);
+  //   });
 
-    const isRegexName = (userSignupData.userName.length > 1 && userSignupData.userName.length < 6) || userSignupData.userName.length === 0;
-    const isRegexNameMessage = isRegexName ? '' : "2~5자"
-      
-    const isValidSignup = isRegexEmail && isRegexPassword && isRegexPasswordConfirm && isRegexName && userSignupData.userEmail && userSignupData.userPassword && userSignupData.userPasswordConfirm && userSignupData.userName;
-  
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(userSignupData);
-        alert(`${userSignupData.userName}님 회원가입이 완료되었습니다 !`);
-    };
+  const emailRegex =
+    /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  const isRegexEmail =
+    emailRegex.test(user.userEmail) || user.userEmail.length === 0;
+  const isRegexEmailMessage = isRegexEmail
+    ? ""
+    : "올바른 이메일 형식이 아닙니다.";
+
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+  const isRegexPassword = passwordRegex.test(user.userPassword);
+  const isRegexPasswordMessage = isRegexPassword
+    ? "안전"
+    : "사용불가 (영문자, 숫자, 특수문자 조합/ 8~20자)";
+
+  const isRegexPasswordConfirm =
+    user.userPassword === user.userPasswordConfirm ||
+    user.userPasswordConfirm.length === 0;
+  const isRegexPasswordConfirmMessage = isRegexPasswordConfirm
+    ? ""
+    : "비밀번호가 일치하지 않습니다.";
+
+  const isRegexName =
+    (user.userName.length > 1 && user.userName.length < 6) ||
+    user.userName.length === 0;
+  const isRegexNameMessage = isRegexName ? "" : "2~5자";
+
+  const isValidSignup =
+    isRegexEmail &&
+    isRegexPassword &&
+    isRegexPasswordConfirm &&
+    isRegexName &&
+    user.userEmail &&
+    user.userPassword &&
+    user.userPasswordConfirm &&
+    user.userName;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate("/signupsecond");
+  }
   return (
     <div className="body">
-        <section className="signup-form">
-            <h1 style={{size:20}}>회원가입</h1>
-            <form>
-                <Formbox htmlFor="userEmail" name="이메일*" type="email" title="이메일" onChange={onChangeInput} maxLength='30' message={isRegexEmailMessage}/>
-                <Formbox htmlFor="userPassword" name="비밀번호*" type="password"  title="비밀번호(숫자, 영문자, 특수문자 조합/ 8~20자)" onChange={onChangeInput} maxLength='20'/>
-                {userSignupData.userPassword.length > 0 &&<div className={`message ${isRegexPassword ? 'success' : 'error'}`}>{isRegexPasswordMessage}</div>}
-                <Formbox htmlFor="userPasswordConfirm" name="비밀번호 확인*" type="password" title="비밀번호 확인" onChange={onChangeInput} maxLength='20' message={isRegexPasswordConfirmMessage}/>
-                <Formbox htmlFor="userName" name="이름*" type="text" title="이름" onChange={onChangeInput} maxLength="10" message={isRegexNameMessage}/>
-                <div className="birthdate-form">
-                    <label htmlFor="userBirthdate">생년월일</label>
-                    <br></br> 
-                    <input type="number" id="userBirthdateYear" title="년" placeholder="년" className="year" onChange={onChangeInput} autoComplete='off'></input>
-                    <input type="number" id="userBirthdateMonth" title="월" placeholder="월" className="month" onChange={onChangeInput} autoComplete='off'></input>
-                    <input type="number" id="userBirthdateDay" title="일" placeholder="일" className="day" onChange={onChangeInput} autoComplete='off'></input>
-                </div>
-                <div className="formbox">
-                    <label htmlFor="userSex">성별</label>
-                    <select id="userSex" onChange={onChangeInput}>
-                        <option value="">성별</option>
-                        <option value="man">남자</option>
-                        <option value="woman">여자</option>
-                    </select>
-                </div>
-                <Formbox htmlFor="userPhone" name="휴대전화" type="number" title="전화번호" onChange={onChangeInput}/>
-                <Button id="submitRegister" onClick={(e)=>handleSubmit(e)} condition={isValidSignup} disabled={!isValidSignup}/>
-            </form>
-        </section>
+      <section className="signup-form">
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+          <GoogleLogin
+            onSuccess={responseGoogle}
+            onError={responseGoogle}
+            cookiePolicy="single_host_origin"
+          />
+        </GoogleOAuthProvider>
+        <h1 style={{ size: 20 }}>회원가입</h1>
+        <form>
+          <Formbox
+            htmlFor="userEmail"
+            name="이메일*"
+            type="email"
+            title="이메일"
+            onChange={onChangeInput}
+            maxLength="30"
+            message={isRegexEmailMessage}
+          />
+          <Formbox
+            htmlFor="userPassword"
+            name="비밀번호*"
+            type="password"
+            title="비밀번호(숫자, 영문자, 특수문자 조합/ 8~20자)"
+            onChange={onChangeInput}
+            maxLength="20"
+          />
+          {user.userPassword.length > 0 && (
+            <div className={`message ${isRegexPassword ? "success" : "error"}`}>
+              {isRegexPasswordMessage}
+            </div>
+          )}
+          <Formbox
+            htmlFor="userPasswordConfirm"
+            name="비밀번호 확인*"
+            type="password"
+            title="비밀번호 확인"
+            onChange={onChangeInput}
+            maxLength="20"
+            message={isRegexPasswordConfirmMessage}
+          />
+          <Formbox
+            htmlFor="userName"
+            name="이름*"
+            type="text"
+            title="이름"
+            onChange={onChangeInput}
+            maxLength="10"
+            message={isRegexNameMessage}
+          />
+          <div className="birthdate-form">
+            <label htmlFor="userBirthdate">생년월일</label>
+            <br></br>
+            <input
+              type="number"
+              id="userBirthdateYear"
+              title="년"
+              placeholder="년"
+              className="year"
+              onChange={onChangeInput}
+              autoComplete="off"
+            ></input>
+            <input
+              type="number"
+              id="userBirthdateMonth"
+              title="월"
+              placeholder="월"
+              className="month"
+              onChange={onChangeInput}
+              autoComplete="off"
+            ></input>
+            <input
+              type="number"
+              id="userBirthdateDay"
+              title="일"
+              placeholder="일"
+              className="day"
+              onChange={onChangeInput}
+              autoComplete="off"
+            ></input>
+          </div>
+          <div className="formbox">
+            <label htmlFor="userSex">성별</label>
+            <select id="userSex" onChange={onChangeInput}>
+              <option value="">성별</option>
+              <option value="man">남자</option>
+              <option value="woman">여자</option>
+            </select>
+          </div>
+          <Formbox
+            htmlFor="userPhone"
+            name="휴대전화"
+            type="number"
+            title="전화번호"
+            onChange={onChangeInput}
+          />
+          <Button
+            id="submitRegister"
+            onClick={e => handleSubmit(e)}
+            condition={isValidSignup}
+            disabled={!isValidSignup}
+          />
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
-
+export default Signup;
--- web/src/pages/SignupSecond.jsx
+++ web/src/pages/SignupSecond.jsx
@@ -1,98 +1,320 @@
-import React, { useState, useEffect } from 'react'
+import { UserContext } from "../App";
+import React, { useState, useEffect, useContext } from "react";
 
-import Button from '../components/Button';
-import Formbox from '../components/Formbox';
+import Button from "../components/Button";
+import Formbox from "../components/Formbox";
 
-import '../components/signupsecond.css'
-
-
+import "../components/signupsecond.css";
 
 const SignupSecond = () => {
-  const [userSignupData, setUserSignupData] = useState({
-    userEmail: '',
-    userPassword: '',
-    userPasswordConfirm: '',
-    userName: '',
-    userBirthdateYear: '',
-    userBirthdateMonth: '',
-    userBirthdateDay: '',
-    userSex: '',
-    userPhone: '',
+  // const [userSignupData, setUserSignupData] = useState({
+  //   userEmail: "",
+  //   userPassword: "",
+  //   userPasswordConfirm: "",
+  //   userName: "",
+  //   userBirthdateYear: "",
+  //   userBirthdateMonth: "",
+  //   userBirthdateDay: "",
+  //   userSex: "",
+  //   userPhone: "",
 
-    userPicture: '',
-    userNickname: '',
-    userCollege: '',
-    userSkill: '',
-    userJob: '',
-    userExperience: '',
-    userCareer: '',
-    userIntroduction: '',
-    userBlog: '',
-  });
+  //   userPicture: "",
+  //   userNickname: "",
+  //   userCollege: "",
+  //   userSkill: "",
+  //   userJob: "",
+  //   userExperience: "",
+  //   userCareer: "",
+  //   userIntroduction: "",
+  //   userBlog: "",
+  // });
+  const { user, setUser } = useContext(UserContext);
 
-  const onChangeInput = (e) => {
-      const { id, value } = e.target;
-      setUserSignupData((prev) => ({
-          ...prev,
-          [id]: value,
-      }));
+  const onChangeInput = e => {
+    const { id, value } = e.target;
+    setUser(prev => ({
+      ...prev,
+      [id]: value,
+    }));
   };
 
   useEffect(() => {
-      console.log(userSignupData)
-  })
+    console.log(user);
+  });
 
-  const isValidSignup = 0
+  const isValidSignup = 0;
 
   return (
     <div className="body">
-        <section className="signup-form">
-            <h1 style={{size:20}}>회원가입2</h1>
-            <form method="post" encType="multipart/form-data">
-              <div className="userPicture">
-                <label htmlFor="userPicture">프로필 사진*</label>
-                <input type="file" id="userPicture" name="chooseFile" accept="image/*" title="프로필 사진" onChange={onChangeInput}/>
+      <section className="signup-form">
+        <h1 style={{ size: 20 }}>회원가입2</h1>
+        <form method="post" encType="multipart/form-data">
+          <div className="userPicture">
+            <label htmlFor="userPicture">프로필 사진*</label>
+            {user.userPicture !== "" ? (
+              <div>
+                <img src={user.userPicture} alt="user" />
               </div>
-              <Formbox htmlFor="userNickname" name="닉네임*" type="text" title="닉네임" onChange={onChangeInput}/>
-              <Formbox htmlFor="userCollege" name="학력* (학교/캠퍼스/학과/학번)" type="text" title="학력" onChange={onChangeInput}/>
-              <div className="skill-form">
-                <label htmlFor="userSkill">기술 스택*</label>
-                <br></br>
-                <button type="button" id="userSkill" title="프론트엔드" className="frontend" onChange={onChangeInput} onClick={() => console.log("1")}>Html / CSS</button>
-                <button type="button" id="userSkill" title="백엔드" className="backend" onChange={onChangeInput}>Javascript</button>
-                <button type="button" id="userSkill" title="풀스택" className="fullStack" onChange={onChangeInput}>Typescript</button>
-                <button type="button" id="userSkill" title="프론트엔드" className="frontend" onChange={onChangeInput}>Java</button>
-                <button type="button" id="userSkill" title="백엔드" className="backend" onChange={onChangeInput}>Python</button>
-                <button type="button" id="userSkill" title="풀스택" className="fullStack" onChange={onChangeInput}>PHP</button>
-                <button type="button" id="userSkill" title="프론트엔드" className="frontend" onChange={onChangeInput}>Ruby</button>
-                <button type="button" id="userSkill" title="백엔드" className="backend" onChange={onChangeInput}>Go</button>
-                <button type="button" id="userSkill" title="풀스택" className="fullStack" onChange={onChangeInput}>Rust</button>
-                <button type="button" id="userSkill" title="프론트엔드" className="frontend" onChange={onChangeInput}>C</button>
-                <button type="button" id="userSkill" title="백엔드" className="backend" onChange={onChangeInput}>C++</button>
-                <button type="button" id="userSkill" title="풀스택" className="fullStack" onChange={onChangeInput}>C#</button>
-              </div>
-              <div className="job-form">
-                <label htmlFor="userJob">현 직군 / 희망 직무*</label>
-                <br></br>
-                <button type="submit" id="userJob" title="프론트엔드" className="frontend" onChange={onChangeInput}>프론트엔드</button>
-                <button type="submit" id="userJob" title="백엔드" className="backend" onChange={onChangeInput}>백엔드</button>
-                <button type="submit" id="userJob" title="풀스택" className="fullStack" onChange={onChangeInput}>풀스택</button>
-                <button type="submit" id="userJob" title="프론트엔드" className="frontend" onChange={onChangeInput}>데이터베이스</button>
-                <button type="submit" id="userJob" title="백엔드" className="backend" onChange={onChangeInput}>머신러닝</button>
-                <button type="submit" id="userJob" title="풀스택" className="fullStack" onChange={onChangeInput}>모바일 앱</button>
-                <button type="submit" id="userJob" title="프론트엔드" className="frontend" onChange={onChangeInput}>QA엔지니어</button>
-                <button type="submit" id="userJob" title="백엔드" className="backend" onChange={onChangeInput}>시스템엔지니어</button>
-                <button type="submit" id="userJob" title="풀스택" className="fullStack" onChange={onChangeInput}>보안엔지니어</button>
-              </div>
-              <Formbox htmlFor="userExperience" name="프로젝트 경험 / 실무 경험" type="text" title="경험" onChange={onChangeInput}/>
-              <Formbox htmlFor="userCareer" name="수상 경력 / 자격증" type="text"  title="경력" onChange={onChangeInput}/>
-              <Formbox htmlFor="userIntroduction" name="세 줄 자기 소개" type="text" title="자기소개" onChange={onChangeInput}/>
-              <Formbox htmlFor="userBlog" name="기술 블로그 / 깃허브" type="url" title="블로그" onChange={onChangeInput}/>
-              <Button id="submitRegister" condition={isValidSignup} disabled={!isValidSignup}/>
-            </form>
-        </section>
+            ) : (
+              <input
+                type="file"
+                id="userPicture"
+                name="chooseFile"
+                accept="image/*"
+                title="프로필 사진"
+                onChange={onChangeInput}
+              />
+            )}
+          </div>
+          <Formbox
+            htmlFor="userNickname"
+            name="닉네임*"
+            type="text"
+            title="닉네임"
+            onChange={onChangeInput}
+          />
+          <Formbox
+            htmlFor="userCollege"
+            name="학력* (학교/캠퍼스/학과/학번)"
+            type="text"
+            title="학력"
+            onChange={onChangeInput}
+          />
+          <div className="skill-form">
+            <label htmlFor="userSkill">기술 스택*</label>
+            <br></br>
+            <button
+              type="button"
+              id="userSkill"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+              onClick={() => console.log("1")}
+            >
+              Html / CSS
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              Javascript
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              Typescript
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              Java
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              Python
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              PHP
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              Ruby
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              Go
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              Rust
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              C
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              C++
+            </button>
+            <button
+              type="button"
+              id="userSkill"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              C#
+            </button>
+          </div>
+          <div className="job-form">
+            <label htmlFor="userJob">현 직군 / 희망 직무*</label>
+            <br></br>
+            <button
+              type="submit"
+              id="userJob"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              프론트엔드
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              백엔드
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              풀스택
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              데이터베이스
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              머신러닝
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              모바일 앱
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="프론트엔드"
+              className="frontend"
+              onChange={onChangeInput}
+            >
+              QA엔지니어
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="백엔드"
+              className="backend"
+              onChange={onChangeInput}
+            >
+              시스템엔지니어
+            </button>
+            <button
+              type="submit"
+              id="userJob"
+              title="풀스택"
+              className="fullStack"
+              onChange={onChangeInput}
+            >
+              보안엔지니어
+            </button>
+          </div>
+          <Formbox
+            htmlFor="userExperience"
+            name="프로젝트 경험 / 실무 경험"
+            type="text"
+            title="경험"
+            onChange={onChangeInput}
+          />
+          <Formbox
+            htmlFor="userCareer"
+            name="수상 경력 / 자격증"
+            type="text"
+            title="경력"
+            onChange={onChangeInput}
+          />
+          <Formbox
+            htmlFor="userIntroduction"
+            name="세 줄 자기 소개"
+            type="text"
+            title="자기소개"
+            onChange={onChangeInput}
+          />
+          <Formbox
+            htmlFor="userBlog"
+            name="기술 블로그 / 깃허브"
+            type="url"
+            title="블로그"
+            onChange={onChangeInput}
+          />
+          <Button
+            id="submitRegister"
+            condition={isValidSignup}
+            disabled={!isValidSignup}
+          />
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default SignupSecond
+export default SignupSecond;
